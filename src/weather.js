d46d47d092a6637d37d37896549574f1d65e0f50
Clemmiemjr <clemmiemjr@gmail.com>
2023-12-01 22:20:54 +0000

    Added API to call weather data and replace h2

--- src/weather.js
+++ src/weather.js
@@ -1,8 +1,22 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temp-number");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city-name");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "8t94954492f3aa97ffbbff3b440e4odd";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSumbit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-search");
-  let cityElement = document.querySelector("#city-name");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFieldElement = document.querySelector("#search-field");
