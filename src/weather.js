01e12070d7730de857f871d80c2a08d32c63f60d
Clemmiemjr <clemmiemjr@gmail.com>
2023-12-02 16:58:16 +0000

    Add date array

--- src/weather.js
+++ src/weather.js
@@ -8,11 +8,31 @@ function updateWeather(response) {
   let timeElement = document.querySelector("#date");
   let date = new Date(response.data.time * 1000);
   cityElement.innerHTML = response.data.city;
-  descriptionElement.innerHTML = response.data.condition.description;
+  descriptionElement.innerHTML = `, ${response.data.condition.description}`;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed} km/ph`;
-  timeElement.innerHTML = `${date.getDay()} ${date.getHours()}:${date.getMinutes()}`;
+  timeElement.innerHTML = formatDate(date);
   temperatureElement.innerHTML = `${Math.round(temperature)}c`;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
