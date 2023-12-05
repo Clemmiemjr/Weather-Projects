fcf940989b5a529108400251553cdb6386a2d318
Clemmiemjr <clemmiemjr@gmail.com>
2023-12-05 21:51:28 +0000

    Add loop to generate forecast days from array

--- src/weather.js
+++ src/weather.js
@@ -51,5 +51,32 @@ function handleSearchSumbit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#weather-forecast");
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur"];
+  let forecastHTML = "";
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+  <div class="row-5">
+    <div class="col-2">
+      <div class="weather-forecast-date">${day}</div>
+      <div class="weather-forecast-icon">🌥</div>
+      <div class="weather-forecast-temperature">
+        <span class="weather-forecast-temperature-min">8</span>
+        <span class="weather-forecast-temperature-max">12</span>
+      </div>
+    </div>
+  </div>`;
+  });
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
 let searchFieldElement = document.querySelector("#search-field");
 searchFieldElement.addEventListener("submit", handleSearchSumbit);
+
+displayForecast();
